4c99e1f8b3d10e6889c45e5d9f76ef97c1e6d52b
brijesh rupavatiya <brijesh.rupavatiya@kalkani.in>
2025-03-10 11:03:04 +0530

    Feature 1.3 Email verification before accessing the dashboard | Restrict login until the email is verified

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'Hey this new API is working!']);
@@ -11,7 +12,7 @@ Route::get('/test', function () {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-//varify the users email after register
+//varify the users email after register (API response)
 Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
 
 Route::post('/email/resend', [AuthController::class, 'resendVerificationEmail'])->middleware('auth:sanctum');
