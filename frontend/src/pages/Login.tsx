0219b308b8fe337464b13710aba2e59aad15459a
ks-brijeshr <brijesh.rupavatiya@kalkani.in>
2025-04-01 17:45:40 +0530

    Solve error of Link in login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import Navbar from "../components/Navbar";
